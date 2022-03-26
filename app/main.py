f87db6dab0a229ad0f6f438c0ed5f86548f447e0
Verevkin-A <sandyverevkin@gmail.com>
2022-03-26 15:44:38 +0100

    feat: rewrite requests for database

--- app/main.py
+++ app/main.py
@@ -9,12 +9,9 @@ from psycopg2.extras import RealDictCursor
 # TODO documentation, header
 app = FastAPI()
 
-all_posts = [{"title": "post1", "content": "post1 content", "id": 1},
-             {"title": "post2", "content": "post2 content", "id": 2}]
-
 while True:
     try:
-        conn = psycopg2.connect(host="localhost", database="social_media_db", user="postgres", password="mysecretpassword",
+        conn = psycopg2.connect(host="localhost", database="social_media_db", user="postgres", password="2682",
                                 cursor_factory=RealDictCursor)  # TODO move information into .env
         cur = conn.cursor()
         print("Successful database connection")
@@ -31,20 +28,6 @@ class Post(BaseModel):
     published: bool = True
 
 
-def find_by_id(id_: int) -> dict | None:
-    for post in all_posts:
-        if post["id"] == id_:
-            return post
-    return None
-
-
-def find_post_index(id_: int) -> int | None:
-    for index, post in enumerate(all_posts):
-        if post["id"] == id_:
-            return index
-    return None
-
-
 @app.get("/")
 def root():
     return {"message": "Hi :)"}
@@ -52,48 +35,68 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": all_posts}
+    # fetch all existing posts
+    cur.execute("SELECT * FROM posts")
+    posts = cur.fetchall()
+    return {"all posts": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    post_dict: dict = dict(post)
-    post_dict["id"] = randrange(1000)
-    all_posts.append(post_dict)
-    return {"new post": post_dict}
+    # insert single raw, avoiding SQL injections by inserting without f-string
+    cur.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
+                (post.title, post.content, post.published))
+    # get created post for returning
+    created_post = cur.fetchone()
+    # commit changes into database
+    conn.commit()
+    return {"new post": created_post}
 
 
 @app.get("/posts/latest")
 def get_latest_post():
-    return {"post_detail": all_posts[-1]}
+    # get last row from posts
+    cur.execute("SELECT * FROM posts ORDER BY id DESC LIMIT 1")
+    latest_post = cur.fetchone()
+    return {"latest post": latest_post}
 
 
 @app.get("/posts/{id_}")
 def get_post(id_: int):
-    post = find_by_id(id_)
-    if post is None:
+    # find post by id, avoiding SQL injections by inserting without f-string
+    cur.execute("SELECT * FROM posts WHERE id = %s", str(id_))
+    found_post = cur.fetchone()
+    # return 404 if post was not found
+    if found_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post was not found (id: {id_})")
-    return {"post_detail": post}
+    return {"found post": found_post}
 
 
 @app.delete("/posts/{id_}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id_: int):
-    post_index = find_post_index(id_)
-    if post_index is None:
+    # delete post by id, avoiding SQL injections by inserting without f-string
+    cur.execute("DELETE FROM posts WHERE id = %s RETURNING *", str(id_))
+    deleted_post = cur.fetchone()
+    # commit changes into database
+    conn.commit()
+    # return 404 if post was not found
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post was not found (id: {id_})")
-    all_posts.pop(post_index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id_}")
 def update_post(id_: int, post: Post):
-    post_index = find_post_index(id_)
-    if post_index is None:
+    # updated post by id, avoiding SQL injections by inserting without f-string
+    cur.execute("UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *",
+                (post.title, post.content, post.published, str(id_)))
+    updated_post = cur.fetchone()
+    # commit database changes
+    conn.commit()
+    # return 404 if post was not found
+    if updated_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post was not found (id: {id_})")
-    post_dict = dict(post)
-    post_dict["id"] = id_
-    all_posts[post_index] = post_dict
-    return {"post_detail": post_dict}
+    return {"updated post": updated_post}
