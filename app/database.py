f47f967119a7c08f286634100e7fcea5d091b692
Verevkin-A <sandyverevkin@gmail.com>
2022-04-01 21:53:10 +0200

    feat: uri testing

--- app/database.py
+++ app/database.py
@@ -11,11 +11,12 @@ host = settings.host
 port = settings.port
 db_name = settings.db_name
 
-uri = settings.database_url
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-SQLALCHEMY_DB_URL = uri
-# SQLALCHEMY_DB_URL = f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
+# uri = settings.database_url
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
+# SQLALCHEMY_DB_URL = uri
+SQLALCHEMY_DB_URL = f"postgresql://{settings.db_user}:{settings.db_password}" \
+                    f"@{settings.host}:{settings.port}/{settings.db_name}"
 engine = create_engine(SQLALCHEMY_DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
